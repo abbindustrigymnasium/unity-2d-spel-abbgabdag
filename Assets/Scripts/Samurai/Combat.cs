b1630840133978b4a11b8b2fe18161e4b90572df
abbgabdag <abbgabdag@gmail.com>
2022-11-11 19:54:07 +0100

    Combat variables
    combat variables

--- Assets/Scripts/Samurai/Combat.cs
+++ Assets/Scripts/Samurai/Combat.cs
@@ -4,15 +4,25 @@ using UnityEngine;
 
 public class Combat : MonoBehaviour
 {
-    // Start is called before the first frame update
+
+    private Animator animator;
+    private float attackDmg;
+    private float attackDelay;
+    Movement 
+
     void Start()
     {
+        animator = GetComponent<Animator>();
+        attackDmg = 14f;
         
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+
+        }
     }
 }
